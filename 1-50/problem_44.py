fd116264b0f567b1f19973c143c5fa1e0bb3f625
yann <me.yan.xia@qq.com>
2015-06-10 10:29:48 +0800

    44

--- 1-50/problem_44.py
+++ 1-50/problem_44.py
@@ -3,25 +3,29 @@ __author__ = 'Yann'
 import math
 
 
-def triangle(n):
-    return int(n * (n + 1) / 2)
-
-
 def pentagonal(n):
     return int(n * (3 * n - 1) / 2)
 
 
-def hexagonal(n):
-    return int(n * (2 * n - 1))
-
-
-def is_triangle(n):
-    return int(n * (n + 1) / 2)
-
-
 def is_pentagonal(n):
-    return int(n * (3 * n - 1) / 2)
+    return str(get_x(3, -1, (-n) * 2)).split('.')[1] == '0'
 
 
 def get_x(a, b, c):
-    return (-b + abs(math.sqrt(math.pow(b, 2) - 4 * a * c))) / 2 * a
+    return (-b + abs(math.sqrt(math.pow(b, 2) - 4 * a * c))) / (2 * a)
+
+
+notFound = True
+result = 0
+i = 1
+while notFound:
+    i += 1
+    n = pentagonal(i)
+    for j in range(1, i):
+        m = pentagonal(j)
+        if is_pentagonal(n - m) and is_pentagonal(n + m):
+            result = n - m
+            notFound = False
+            break
+
+print(result)
