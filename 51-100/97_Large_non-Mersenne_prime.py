6158261d4975a0e58401de3ae29512e3dfab3220
yann <me.yan.xia@qq.com>
2015-07-18 14:35:33 +0800

    97

--- 51-100/97_Large_non-Mersenne_prime.py
+++ 51-100/97_Large_non-Mersenne_prime.py
@@ -1,1 +1,15 @@
 __author__ = 'Yann'
+
+mod_number = 10000000000
+product = 1
+
+i = 1
+
+while i <= 7830457:
+    product *= 2
+    product %= mod_number
+    i += 1
+product = 28433 * product + 1
+product = str(product)
+size = len(product)
+print(product[size - 10:size])
