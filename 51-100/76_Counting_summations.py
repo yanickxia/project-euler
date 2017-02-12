2a8d84cfd806b8dc2f767733a63a87bad5a59e1c
yann <me.yan.xia@qq.com>
2017-02-12 14:26:26 +0800

    Add 76

--- 51-100/76_Counting_summations.py
+++ 51-100/76_Counting_summations.py
@@ -1,35 +1,17 @@
 # -*- coding:utf-8 -*-
 
 
-MAX = 4
-COUNT = 0
-n_array = [0] * MAX
 
 
-def put_n_in_index(n, array):
-    if sum(array) == MAX:
-        global COUNT
-        COUNT += 1
-        return
-    """
-    before n is set
-    :param n: int
-    :return:
-    """
-    max_n = MAX - sum(array[0:n])
-    for i in range(array[n - 1], 0, -1):
-        if i > max_n:
-            continue
 
-        array[n] = i
-        put_n_in_index(n + 1, array)
+#Project Euler Problem 76
 
+target = 100
+ns = range(1, target)
+ways = [1] + [0]*target
 
-def loop():
-    for i in range(MAX - 1, 0, -1):
-        n_array[0] = i
-        put_n_in_index(1, n_array)
+for n in ns:
+    for i in range(n, target+1):
+        ways[i] += ways[i-n]
 
-
-loop()
-print(COUNT)
+print("Number of ways", target, "can be written as a \nsum of at least two positive integers:", ways[target])
